a242d403c13b165ac4f155206534418a26d0760d
Pranav Khavare <pranavkhavare@gmail.com>
2022-03-02 15:46:41 -0500

    Added Project Validations

---
+++ PMToolSpringBoot/src/main/java/com/pranavkhavare/pmt/utility/ErrorUtility.java
@@ -0,0 +1,35 @@
+package com.pranavkhavare.pmt.utility;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Contains static methods to handle errors.
+ * Can also be made as a service.
+ */
+public class ErrorUtility {
+
+    /**
+     * Identifies the error fields and cause of the error.
+     *
+     * @param result BindingResult object
+     * @return Map with error fields and what caused the error.
+     */
+    public static ResponseEntity<Map<String, String>> FieldErrorMap(BindingResult result) {
+        if (result.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            List<FieldError> errorList = result.getFieldErrors();
+            for (FieldError error : errorList) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+        return null;
+    }
+}
--- PMToolSpringBoot/src/main/java/com/pranavkhavare/pmt/web/ProjectController.java
+++ PMToolSpringBoot/src/main/java/com/pranavkhavare/pmt/web/ProjectController.java
@@ -2,13 +2,18 @@ package com.pranavkhavare.pmt.web;
 
 import com.pranavkhavare.pmt.domain.Project;
 import com.pranavkhavare.pmt.services.ProjectService;
+import com.pranavkhavare.pmt.utility.ErrorUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -19,12 +24,19 @@ public class ProjectController {
 
     /**
      * Creates new Project in DB and return an HTTP status.
+     *
      * @param project
      * @return
+     * @Valid removes 500 error and descriptive error
+     * BindingResult interface invokes the validator on the object to check if there are error
      */
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+        ResponseEntity<Map<String, String>> errorMap = ErrorUtility.FieldErrorMap(result);
+        if (errorMap != null) {
+            return errorMap;
+        }
         Project project1 = projectService.saveOrUpdateProject(project);
-        return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
+        return new ResponseEntity<>(project1, HttpStatus.CREATED);
     }
 }
